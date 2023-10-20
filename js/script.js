cb3772a40250a6c7ab8baccc9c8792f89b2bcd0c
diego <dmbruno61@gmail.com>
2023-10-20 19:03:20 -0300

    fix form

--- js/script.js
+++ js/script.js
@@ -15,44 +15,46 @@ const destinos = [
         description: `Brasil es conocido por sus playas, que atraen a turistas de todo el mundo. Las playas de Brasil son famosas por su belleza natural, su ambiente relajado y su vibrante cultura. 
         `,
         thumbnail: `https://images.unsplash.com/photo-1473116763249-2faaef81ccda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1792&q=80`,
-        price: "USD " + 900
+        price: 900
     },
     {
         id: `Mexico`,
         title: `Rivera Maya`,
         description: `La Riviera Maya es una hermosa región costera en México que cuenta con muchas playas impresionantes`,
         thumbnail: `https://images.unsplash.com/photo-1616423841125-8307665a0469?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80`,
-        price: "USD " + 1000
+        price: 1000
     },
     {
         id: `Nepal`,
         title: `Montañas en Nepal`,
         description: `Nepal es un país multicultural y multilingüe ubicado en Asia del Sur, entre India y China `,
         thumbnail: `https://images.unsplash.com/photo-1571401835393-8c5f35328320?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80`,
-        price: "USD " + 1200
+        price:  1200
     },
     {
         id: `Tailandia`,
         title: `Montañas en Tailandia`,
         description: `Tailandia es un país conocido por sus hermosas playas, que atraen a turistas de todo el mundo`,
         thumbnail: `https://images.unsplash.com/photo-1528181304800-259b08848526?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80`,
-        price: "USD " + 1300
+        price:  1300
     },
     {
         id: `New York`,
         title: `NYC`,
         description: `Nueva York es una ciudad vibrante y emocionante que atrae a turistas de todo el mundo.`,
         thumbnail: `https://images.unsplash.com/photo-1568515387631-8b650bbcdb90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80`,
-        price: "USD " + 1300
+        price:  1300
     },
     {
         id: `Paris`,
         title: `Paris`,
         description: `París es una ciudad hermosa y vibrante que atrae a turistas de todo el mundo. La ciudad es conocida por su rica historia, su cultura vibrante y su arquitectura impresionante`,
         thumbnail: `https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80`,
-        price: "USD " + 1300
+        price: 1300
     }
 ]
+
+
 
 
 //aca cremaos e insertamos por js una card con la info de los diferentes destinos
@@ -61,9 +63,11 @@ for (const destino of destinos) {
     contenedor.innerHTML += `
     <article class = "contenedor-card">
         <img class="contenedor-img" src= ${destino.thumbnail} alt = "imagen sobre ${destino.title}">
+        
     <div class="contenedor-texto">
         <h2>${destino.title}</h2>
         <p>${destino.description}</p>
+        <p class="precioM">USD</p>
         <b class="precio">${destino.price}</b>
         <div class="contenedor-botones">
         <button type="submit" class="envio-mail" data-id=${destino.id}>Enviar Mail</button>
@@ -72,6 +76,7 @@ for (const destino of destinos) {
     </article>
     `;
 }
+
 document.body.append(contenedor);
 
 
@@ -90,14 +95,20 @@ function mandarMail() {
     <div class="formulario-correo">
     <form action="" id="form-viaje">
         <label for="nombre" class="nombre">Nombre</label>
-        <input type="text" id="nombre" placeholder="Ingresa tu nombre...">
+        <input type="text" id="nombre" placeholder="Ingresa tu nombre..." required>
         <label for="apellido" class="nombre">Apellido</label>
-        <input type="text" id="apellido" placeholder="Ingresa tu apellido...">
+        <input type="text" id="apellido" placeholder="Ingresa tu apellido..." required>
+        <label for="cantidad" class="nombre">Cantidad de pasajeros</label>
+        <input type="number" id="number" placeholder="Ingrese un numero" required>
+        <label for="desde" class="nombre">Salida</label>
+        <input type="date" id="nombre" required>
+        <label for="hasta" class="nombre">Regreso</label>
+        <input type="date" id="nombre" required>
         <label for="E-mail" class="nombre">E-mail</label>
-        <input type="text" id="e-mail" placeholder="Ingresa tu correo electronico...">
-        <label for="correo" class="nombre">Escribinos tu consulta</label>
-        <input type="text" id="correo" placeholder="Gracais por elegirnos, ingresa tu consulta y te responderemos via mail a la brevedad.">
-        <button>Enviar Consulta</button>
+        <input type="text" id="e-mail" placeholder="Ingresa tu correo electronico..." required>
+        <label for="correo" class="nombre">Mandanos aca tus dudas...</label>
+        <input type="text" id="correo" placeholder="Gracais por elegirnos, ingresa tu consulta y te responderemos via mail a la brevedad." required>
+        <button class="botonMail">Enviar Consulta</button>
     </form>
 </div>
 `
