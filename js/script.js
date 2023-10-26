a91cf2c9a4cc3e4f4afa81d38f05afa5f7e6084d
diego <dmbruno61@gmail.com>
2023-10-26 12:28:03 -0300

    fix Dom Css2

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@ const destinos = [
     {
         id: `Paris`,
         title: `Paris`,
-        description: `París es una ciudad hermosa y vibrante que atrae a turistas de todo el mundo. La ciudad es conocida por su rica historia, su cultura vibrante y su arquitectura impresionante`,
+        description: `París es una ciudad hermosa y vibrante que atrae a turistas de todo el mundo. La ciudad es conocida por su rica historiay su arquitectura impresionante`,
         thumbnail: `https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80`,
         price: 1300
     }
